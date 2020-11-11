6c904e114d5cbe7f9a932bef19819a42e8286934
snganyon@terpmail.umd.edu <snganyone1@gmail.com>
2020-11-10 22:37:10 -0800

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr){
+  let result = arr.find(({result}) => result === "W");
+  if (result){
+    return result.year;
+  }
+}
